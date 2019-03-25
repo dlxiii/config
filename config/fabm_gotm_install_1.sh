af3ba0eb93c23964d9f228f79f101c02714950e9
Yulong WANG <32951329+dlxiii@users.noreply.github.com>
2019-03-26 08:58:03 +0900

    Update fabm_gotm_install_1.sh

--- config/fabm_gotm_install_1.sh
+++ config/fabm_gotm_install_1.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 export DATETIME=$(date "+%Y-%m-%d %H:%M:%S")
-export APPDIR=/usr/local
-export FILDIR=$HOME/Downloads
+export COMPILER=gfortran
+export APPDIR=/usr/local/$COMPILER
+export FILEDIR=$HOME/Downloads
+
+if [ $COMPILER = 'gfortran']; then
+	export CC=gcc
+	export CXX=c++
+else
+	export CC=icc -E
+	export CXX=$CC
+fi
+
 
 # install and setup zlib
-cd $FILDIR
+cd $FILEDIR
 tar -xzvf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure --prefix=$APPDIR
@@ -16,10 +26,10 @@ cd ..
 rm -rf zlib-1.2.11
 
 # install and setup hdf5
-cd $FILDIR
+cd $FILEDIR
 tar -xzvf hdf5-1.8.20.tar.gz
 cd hdf5-1.8.20
-./configure --prefix=$APPDIR --with-zlib=/usr/local CC=gcc CXX=c++ --enable-cxx CFLAGS=-O3 CXXFLAGS=-O3 FC=gfortran F77=gfortran F90=gfortran --enable-fortran --enable-fortran2003
+./configure --prefix=$APPDIR --with-zlib=/usr/local CC=$CC CXX=$CXX --enable-cxx CFLAGS=-O3 CXXFLAGS=-O3 FC=$COMPILER F77=$COMPILER F90=$COMPILER --enable-fortran --enable-fortran2003
 make
 make check
 sudo make install
@@ -34,10 +44,10 @@ sudo apt install curl
 sudo apt install libtool
 
 # install netcdf-c
-cd $FILDIR
+cd $FILEDIR
 tar -xzvf netcdf-c-4.6.1.tar.gz
 cd netcdf-c-4.6.1
-LDFLAGS=-L$APPDIR/lib CPPFLAGS=-I$APPDIR/include ./configure --prefix=$APPDIR CC=gcc CXX=c++ CFLAGS=-O3 CXXFLAGS=-O3 FC=gfortran F77=gfortran F90=gfortran --enable-fortran --disable-netcdf-4 --disable-dap
+LDFLAGS=-L$APPDIR/lib CPPFLAGS=-I$APPDIR/include ./configure --prefix=$APPDIR CC=$CC CXX=$CXX CFLAGS=-O3 CXXFLAGS=-O3 FC=$COMPILER F77=$COMPILER F90=$COMPILER --enable-fortran --disable-netcdf-4 --disable-dap
 make
 make check
 sudo make install
@@ -45,10 +55,10 @@ cd ..
 rm -rf netcdf-c-4.6.1
 
 # install netcdf-fortran
-cd $FILDIR
+cd $FILEDIR
 tar -xzvf netcdf-fortran-4.4.4.tar.gz
 cd netcdf-fortran-4.4.4
-LDFLAGS=-L$APPDIR/lib CPPFLAGS=-I$APPDIR/include ./configure --prefix=$APPDIR CC=gcc CXX=c++ CFLAGS=-O3 CXXFLAGS=-O3 FC=gfortran F77=gfortran F90=gfortran
+LDFLAGS=-L$APPDIR/lib CPPFLAGS=-I$APPDIR/include ./configure --prefix=$APPDIR CC=$CC CXX=$CXX CFLAGS=-O3 CXXFLAGS=-O3 FC=$COMPILER F77=$COMPILER F90=$COMPILER
 make
 make check
 sudo make install
