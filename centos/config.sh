9384c8646462b9ffc89d7a8921c305b8bbf4ce66
Yulong WANG <tokyowangyulong@gmail.com>
2019-03-28 20:21:02 +0900

    Update config.sh

--- centos/config.sh
+++ centos/config.sh
@@ -10,6 +10,8 @@ sudo yum -y groupinstall development
 
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 
+sudo yum -y install python-devel
+
 sudo yum -y install python36u
 
 sudo yum -y install python36u-pip
